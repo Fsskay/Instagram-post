92bea810e1b9ed8e890bd4f0ea236a6c4ecb0e5c
Fsskay <fsskay@126.com>
2020-10-18 00:01:43 +0800

    进度: 1.由于提供的接口响应速度太慢,因此将数据保存在本地,使用json-server代替原始接口,使用mock 目前问题: 1.进阶需求图片懒加载尝试使用阿里OSS失败(暂时不考虑该条) 2.数据流并非自上而下 需要使用withContext重构一下 计划: 1.学习使用redux处理数据流(Redux优先级大于router的问题) 2.编辑detail页面(需要image的onclick传入对应的_uid) 3.detail页面的创建与传入的数据,React-router使用(先暂时不考虑router的问题)

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ console.log('axios', axios);
 
 
 // 为给定 ID 的 user 创建请求
-axios.get('https://www.castlery.com/api/story_bloks/social-images')
+axios.get('/story')
     .then(function (response) {
         console.log('fdsfdsfdsaf', response);
     })
